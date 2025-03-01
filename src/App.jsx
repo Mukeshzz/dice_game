588c36b9639e2cef0e831efc854e5c38bc101ab7
Mukeshzz <mukesh7.251097@gmail.com>
2025-03-01 15:08:56 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,32 @@
-import { useState } from "react";
-import axios from "axios";
-import './App.css';
+import "./App.css";
 
-export default function App() {
-  const [balance, setBalance] = useState(1000);
-  const [bet, setBet] = useState(10);
-  const [roll, setRoll] = useState(null);
-  const [hash, setHash] = useState("");
+import React, { useState } from "react";
+import Startgame from "./components/Startgame";
 
-  const rollDice = async () => {
-    if (bet > balance || bet <= 0) {
-      alert("Invalid bet amount!");
-      return;
-    }
+const App = () => {
+  const [start, setStart] = useState(false);
 
-    try {
-      const res = await axios.post("http://localhost:3000/roll-dice", { bet });
-      const { dice, newBalance, hash } = res.data;
-
-      setRoll(dice);
-      setBalance(newBalance);
-      setHash(hash);
-    } catch (err) {
-      console.error("Error rolling dice:", err);
-    }
+  const startGame = () => {
+    setStart(true);
   };
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold">🎲 Provably Fair Dice Game 🎲</h1>
-
-      <p className="mt-4 text-lg">Balance: ${balance}</p>
-
-      <input
-        type="number"
-        value={bet}
-        onChange={(e) => setBet(Number(e.target.value))}
-        className="mt-3 p-2 text-black rounded"
-        min="1"
-        max={balance}
-      />
-
-      <button
-        onClick={rollDice}
-        className="mt-3 bg-blue-500 px-6 py-2 rounded hover:bg-blue-700 transition"
-      >
-        Roll Dice
-      </button>
-
-      {roll !== null && (
-        <div className="mt-5">
-          <p className="text-xl">You rolled: <strong>{roll}</strong></p>
-          <p className="text-sm mt-2 break-all">Hash: {hash}</p>
+    <>
+      {start ? (
+        <Startgame setStart={setStart}/>
+      ) : (
+        <div className="min-h-screen bg-[#003161] flex items-center justify-center gap-6">
+          <img src="dice.png" alt="" />
+          <button
+            className="bg-amber-50 text-3xl rounded p-4 cursor-pointer"
+            onClick={startGame}
+          >
+            Start Game
+          </button>
         </div>
       )}
-    </div>
+    </>
   );
-}
+};
+
+export default App;
---
+++ src/components/Startgame.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function Startgame({setStart}) {
+  const [balance, setBalance] = useState(1000);
+  const [bet, setBet] = useState(10);
+  const [roll, setRoll] = useState(null);
+  const [hash, setHash] = useState("");
+  
+
+ 
+
+  const rollDice = async () => {
+    if (bet > balance || bet <= 0) {
+      alert("Invalid bet amount!");
+      return;
+    }
+
+    try {
+      const res = await axios.post("http://localhost:3000/roll-dice", { bet });
+      const { dice, newBalance, hash } = res.data;
+
+      setRoll(dice);
+      setBalance(newBalance);
+      setHash(hash);
+    } catch (err) {
+      console.error("Error rolling dice:", err);
+    }
+  };
+
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-[#003161] gap-2 ">
+        <h1 className="text-5xl font-bold text-[#FFF4B7]">🎲 Dice Game 🎲</h1>
+
+        <p className="mt-4 text-lg text-white">Balance: ${balance}</p>
+
+        <input
+          type="number"
+          value={bet}
+          onChange={(e) => setBet(Number(e.target.value))}
+          className="mt-3 p-2 rounded border border-white text-white"
+          min="1"
+          max={balance}
+        />
+
+        <button
+          onClick={rollDice}
+          className="mt-3 bg-amber-50 text-3xl rounded p-4 cursor-pointer transition"
+        >
+          Roll Dice
+        </button>
+
+        {roll !== null && (
+          <div className="mt-5  flex justify-center flex-col gap-5 items-center">
+            <p className="text-2xl text-white">
+              You rolled: <strong>{roll}</strong>
+            </p>
+            <p className="text-xl mt-2 break-all text-white">Hash: {hash}</p>
+            <button className="bg-amber-50 text-2xl rounded p-3 cursor-pointer" onClick={ () => setStart(false)}>
+              End Game
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
